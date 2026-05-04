a9f26fcd984e0026ba8f85c4f99f029808caaec8
lexyZzz <akshithkumarpathak@gmail.com>
2026-05-04 10:37:30 +0530

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,6 @@
     <nav class="navbar navbar-dark bg-dark px-3">
       <span class="navbar-brand">Employee Management</span>
 
-```
   <div>
     <router-link to="/" class="btn btn-light me-2">Home</router-link>
     <router-link to="/add" class="btn btn-success">Add</router-link>
@@ -15,7 +14,6 @@
 <div class="container mt-4">
   <router-view />
 </div>
-```
 
   </div>
 </template>
